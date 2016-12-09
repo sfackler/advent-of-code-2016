7cb4d2a377f5e6c13426af98c3a4c525470fbdbf
Steven Fackler <sfackler@gmail.com>
2016-12-08 22:45:33 -0800

    Day 3 part 2

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -1,11 +1,30 @@
 fn main() {
     let input = include_str!("../input.txt");
 
+    let mut a = vec![];
+    let mut b = vec![];
+    let mut c = vec![];
+
     let mut valid = 0;
     for line in input.lines() {
         let mut sides = line.split_whitespace()
             .map(|s| s.parse::<i32>().unwrap())
             .collect::<Vec<_>>();
+        a.push(sides[0]);
+        b.push(sides[1]);
+        c.push(sides[2]);
+
+        sides.sort();
+        if sides[0] + sides[1] > sides[2] {
+            valid += 1;
+        }
+    }
+    println!("{}", valid);
+
+    valid = 0;
+    a.extend(b);
+    a.extend(c);
+    for sides in a.chunks_mut(3) {
         sides.sort();
         if sides[0] + sides[1] > sides[2] {
             valid += 1;
