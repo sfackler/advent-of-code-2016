8299bb5c9d2292dc220eaba30f7cf74dc006ad65
Steven Fackler <sfackler@gmail.com>
2016-12-08 22:35:54 -0800

    Day 2 part 2

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -5,9 +5,15 @@ fn clamp(x: i32, r: Range<i32>) -> i32 {
     cmp::min(r.end - 1, cmp::max(x, r.start))
 }
 
+fn range(x: i32) -> Range<i32> {
+    let size = 2 - (2 - x).abs();
+    2-size..2+size+1
+}
+
 fn main() {
     let input = include_str!("../input.txt");
     let keypad = [["7", "8", "9"], ["4", "5", "6"], ["1", "2", "3"]];
+    let keypad2 = [[" ", " ", "D", " ", " "], [" ", "A", "B", "C", " "], ["5", "6", "7", "8", "9"], [" ", "2", "3", "4", " "], [" ", " ", "1", " ", " "]];
 
     let mut x = 1i32;
     let mut y = 1i32;
@@ -22,6 +28,22 @@ fn main() {
             }
         }
         print!("{}", keypad[y as usize][x as usize])
+    }
+    println!("");
+
+    x = 0;
+    y = 2;
+    for line in input.lines() {
+        for dir in line.trim().chars() {
+            match dir {
+                'U' => y = clamp(y + 1, range(x)),
+                'D' => y = clamp(y - 1, range(x)),
+                'L' => x = clamp(x - 1, range(y)),
+                'R' => x = clamp(x + 1, range(y)),
+                _ => unreachable!(),
+            }
+        }
+        print!("{}", keypad2[y as usize][x as usize]);
     }
     println!("");
 }
